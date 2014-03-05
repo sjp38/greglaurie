196c06e9589037df9a58f4ae864f07e54cdd1d11
SeongJae Park <sj38.park@gmail.com>
2014-03-05 09:25:49 +0900

    implement kill feature on recent apps activity
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- src/org/drykiss/android/app/laurie/RecentAppsActivity.java
+++ src/org/drykiss/android/app/laurie/RecentAppsActivity.java
@@ -4,6 +4,7 @@ package org.drykiss.android.app.laurie;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RecentTaskInfo;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -94,6 +95,8 @@ public class RecentAppsActivity extends Activity {
                 LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 convertView = inflater.inflate(R.layout.recent_app_list, null);
                 TextView tv = (TextView) convertView.findViewById(R.id.recentAppTextView);
+                TextView killButton = (TextView) convertView
+                        .findViewById(R.id.killRecentAppTextView);
                 ImageView icon = (ImageView) convertView.findViewById(R.id.iconImageView);
                 holder = new RecentViewHolder();
                 holder.textView = tv;
@@ -126,6 +129,26 @@ public class RecentAppsActivity extends Activity {
                     }
                 });
 
+                killButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        View convertView = (View) v.getParent();
+                        RecentViewHolder holder = (RecentViewHolder) convertView.getTag();
+                        RecentTag tag = mRecentTags.get(holder.id);
+                        if (tag.intent != null) {
+                            final PackageManager pm = getApplicationContext().getPackageManager();
+                            final ActivityManager am = (ActivityManager) getSystemService(
+                                    Context.ACTIVITY_SERVICE);
+                            am.killBackgroundProcesses(tag.intent.resolveActivity(pm)
+                                    .getPackageName());
+                            notifyDataSetChanged();
+                        } else {
+                            Toast.makeText(RecentAppsActivity.this, R.string.can_not_kill_app,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
                 convertView.setTag(holder);
             } else {
                 holder = (RecentViewHolder) convertView.getTag();
@@ -146,12 +169,35 @@ public class RecentAppsActivity extends Activity {
             final List<ActivityManager.RecentTaskInfo> recentTasks = am.getRecentTasks(
                     MAX_RECENT_TASKS, ActivityManager.RECENT_IGNORE_UNAVAILABLE);
 
+            final List<RunningAppProcessInfo> procs = am.getRunningAppProcesses();
+
             ActivityInfo homeInfo = new Intent(Intent.ACTION_MAIN)
                     .addCategory(Intent.CATEGORY_HOME).resolveActivityInfo(pm, 0);
             mRecentTags.clear();
             final int numTasks = recentTasks.size();
             for (int i = 0; i < numTasks; i++) {
                 ActivityManager.RecentTaskInfo info = recentTasks.get(i);
+                if (info.id == -1) {
+                    Toast.makeText(RecentAppsActivity.this, "dead process " + info.toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+                boolean found = false;
+                for (int j = 0; j < procs.size(); j++) {
+                    final String[] pkgs = procs.get(j).pkgList;
+                    for (int k = 0; k < pkgs.length; k++) {
+                        if (info.baseIntent.resolveActivity(pm).getPackageName().equals(pkgs[k])) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        break;
+                    }
+                }
+                if (!found) {
+                    continue;
+                }
+
                 Intent intent = new Intent(info.baseIntent);
                 if (info.origActivity != null) {
                     intent.setComponent(info.origActivity);
